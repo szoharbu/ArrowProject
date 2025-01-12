a8e78a9d64d00157ee75ec079cb9ff394424ecfc
Zohar Buchris <szoharbu@gmail.com>
2025-01-13 00:32:29 +0200

    add choice API

--- Tool/frontend/AR_API.py
+++ Tool/frontend/AR_API.py
@@ -1,6 +1,7 @@
+from typing import Union, List, Dict, Any, Optional
 from Utils.logger_management import get_logger
 from Utils.configuration_management import get_config_manager
-from Tool.frontend.asm_logger import AsmLogger
+from Tool.frontend import choice, asm_logger
 
 class AR:
     logger = get_logger()
@@ -11,9 +12,17 @@ class AR:
     @staticmethod
     def asm(asm_code:str, comment:str=None):
         # Calls the internal Tool.asm yet expose to users as TG.asm API
-        return AsmLogger.print_asm_line(asm_code, comment)
+        return asm_logger.AsmLogger.print_asm_line(asm_code, comment)
 
     @staticmethod
     def comment(comment:str):
         # Calls the internal Tool.choice yet expose to users as TG.comment API
-        return AsmLogger.print_comment_line(comment)
+        return asm_logger.AsmLogger.print_comment_line(comment)
+
+    @staticmethod
+    def choice(
+            values: Union[Dict[Any, int], List[Any]],
+            name: Optional[str] = None,
+    ) -> Any:
+        # Calls the internal Tool choice yet expose to users as TG.choice API
+        return choice.choice(values, name)
