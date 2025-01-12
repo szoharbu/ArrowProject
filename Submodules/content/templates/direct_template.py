f8607431d863226eff7cb8097761d5b7d124cb5d
Zohar Buchris <szoharbu@gmail.com>
2025-01-13 00:32:29 +0200

    add choice API

--- Submodules/content/templates/direct_template.py
+++ Submodules/content/templates/direct_template.py
@@ -1,5 +1,7 @@
+import random
 from Tool.frontend.AR_API import AR
 
 AR.comment(comment="Direct test start here")
-AR.asm("mov eax, 7", comment=f"moving 7 into eax")
+number = AR.choice(values={7:30, random.randint(2,40):70})
+AR.asm(f"mov eax, {number}", comment=f"moving {number} into eax")
 
