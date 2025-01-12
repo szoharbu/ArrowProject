2b1d37b91630c5e968ceb6759ac6c0f681c7e857
Zohar Buchris <szoharbu@gmail.com>
2025-01-12 19:39:06 +0200

    add logger api

---
+++ Arrow/__init__.py
@@ -0,0 +1,18 @@
+# tool_name/__init__.py
+
+# Metadata
+__version__ = "0.1.0"
+__author__ = "Zohar Buchris"
+__description__ = "A tool for XYZ functionality."
+
+# # Expose key modules or functions
+# from .core import manager, config
+# from .flows import handler
+# from .io import reader, writer
+# from .utils import helper
+
+#
+# # Optional: Initialize logger
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
--- Arrow/main.py
+++ Arrow/main.py
@@ -1,9 +1,19 @@
+import os
 import time
+import traceback
+from Utils.logger_management import get_logger
 
 def main(args=None):
     start_time = time.time()
 
-    print("==== Arrow main")
+    logger = get_logger()
+    log_manager = get_logger(get_manager=True)
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file = os.path.join(base_dir,'output','test.log')
+    log_manager.setup_file_logging(log_file)
+
+    logger.info("==== Arrow main")
 
     try:
         #read_inputs()          # Read inputs, read template, read configuration, ARM/riscv, ...
@@ -11,20 +21,30 @@ def main(args=None):
         #init_section()           # initialize the state, register, memory and other managers.
         #test_section()           # boot, body (foreach core, foreach scenario), test final
 
-        mid_time = time.time()
-        print(f"test generation end after {mid_time-start_time}")
+        logger.info("Test generated successful :)")
+        dump_time(start_time, "Test generation")
 
         #final_section()         # post flows?
 
-
     except Exception as e:
-        end_time = time.time()
-        print(f"test failed after {end_time-start_time}")
+        logger.warning("Test failed :(")
+        dump_time(start_time, "Test total")
+        logger.error(f"Error: {e}")
+        logger.error(traceback.format_exc())
     else:
-        end_time = time.time()
-        print(f"test total end after {end_time-start_time}")
+        # Test was successful
+        dump_time(start_time, "Test total")
+        logger.info("Test was successful :)\n")
+        logger.info("Mission accomplished...")
 
     return True
+
+
+def dump_time(start_time, message_header):
+    logger = get_logger()
+    current_time = time.time()  # Capture current time
+    duration = current_time - start_time # Calculate duration
+    logger.info(f'{message_header} took {duration:.2f} seconds')
 
 
 if __name__ == "__main__":
