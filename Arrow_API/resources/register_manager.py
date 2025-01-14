95cf35fe07126dac6a801830c63c79d024f36646
Zohar Buchris <szoharbu@gmail.com>
2025-01-14 19:32:42 +0200

    FE refactor - expose to external ARROR_API

--- Arrow_API/resources/register_manager.py
+++ Arrow_API/resources/register_manager.py
@@ -1,16 +1,18 @@
+from Tool.state_management import get_current_state as _get_current_state
 
-from Tool.state_management import get_state_manager
-from Tool.register_management import register_manager
+# Expose the classes under the resources package
+__all__ = ["RegisterManager_API"]  # Only export RegisterManager_API
 
 class RegisterManager_API:
+
+    from Tool.register_management import register_manager
 
     @staticmethod
     def get_free_registers() -> list[register_manager.Register]:
         """
         Returns a list of all free registers.
         """
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         return current_state.register_manager.get_free_registers()
 
     @staticmethod
@@ -18,8 +20,7 @@ class RegisterManager_API:
         """
         Returns a list of all reserved registers.
         """
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         return current_state.register_manager.get_used_registers()
 
     @staticmethod
@@ -28,8 +29,7 @@ class RegisterManager_API:
         Selects a random free register, don't mark them as used (reserved = False),
         and returns the selected register. If no available child is found, raise Error
         """
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         return current_state.register_manager.get(reg_name)
 
     @staticmethod
@@ -38,8 +38,7 @@ class RegisterManager_API:
         Selects a random free register, marks them as used (reserved = True),
         and returns the selected register. If no available child is found, raise Error
         """
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         return current_state.register_manager.get_and_reserve()
 
     @staticmethod
@@ -51,8 +50,7 @@ class RegisterManager_API:
         - register (Register): The register to make reserve.
 
         """
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         current_state.register_manager.reserve(register)
 
     @staticmethod
@@ -64,13 +62,11 @@ class RegisterManager_API:
         - register (Register): The register to make free again.
 
         """
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         current_state.register_manager.free(register)
 
     @staticmethod
     def print_reg_status():
-        state_manager = get_state_manager()
-        current_state = state_manager.get_active_state()
+        current_state = _get_current_state()
         current_state.register_manager.print_reg_status()
 
