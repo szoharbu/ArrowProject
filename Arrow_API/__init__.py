8a9ab183d2f54585f8e76d31f74974d1549fcfd3
Zohar Buchris <szoharbu@gmail.com>
2025-01-14 19:32:42 +0200

    FE refactor - expose to external ARROR_API

--- Arrow_API/__init__.py
+++ Arrow_API/__init__.py
@@ -1,8 +1,7 @@
 # __init__.py
 from Arrow_API.flows import AR
 from Arrow_API import resources
-# from .resources import RegisterResource
 # Expose more flows or utilities as needed
 
-__all__ = ["AR"]
+__all__ = ["AR", "resources"]
 #__all__ = ["create_generation", "RegisterResource"]
--- Arrow_API/resources/__init__.py
+++ Arrow_API/resources/__init__.py
@@ -1,6 +1,6 @@
 # resources/__init__.py
-from Arrow_API.resources.register_manager_API import RegisterManager_API as RegisterManager
-# from .memory import MemoryManager
-#
-# # Expose the classes under the resources package
-# __all__ = ["RegisterManager", "MemoryManager"]
+from Arrow_API.resources.register_manager import RegisterManager_API
+from Arrow_API.resources.memory_manager import MemoryManager_API
+
+# Expose the classes under the resources package
+__all__ = ["RegisterManager_API", "MemoryManager_API"]
