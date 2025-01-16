ec00e153b990fba2278127575fcac11347f3b739
Zohar Buchris <szoharbu@gmail.com>
2025-01-16 19:48:56 +0200

    minor fixes

--- Arrow/main.py
+++ Arrow/main.py
@@ -1,12 +1,15 @@
 import time
 import traceback
 from pathlib import Path
-from Utils.arg_parser.arg_parser import parse_arguments
-from Utils.logger_management import get_logger
-from Utils.configuration_management import get_config_manager
 
 def main(args=None):
+
     start_time = time.time()
+
+    from Utils.arg_parser.arg_parser import parse_arguments
+    from Utils.logger_management import get_logger
+    from Utils.configuration_management import get_config_manager
+
     set_basedir_path()
 
     logger = get_logger()
@@ -14,6 +17,7 @@ def main(args=None):
     logger.info("==== Arrow main")
 
     try:
+
         from Tool.stages import input_stage, evaluation_stage, init_stage, test_stage, final_stage
 
         input_stage.read_inputs()          # Read inputs, read template, read configuration, ARM/riscv, ...
@@ -22,20 +26,20 @@ def main(args=None):
         test_stage.test_section()           # boot, body (foreach core, foreach scenario), test final
 
         logger.info("Test generated successful :)")
-        dump_time(start_time, "Test generation")
+        logger.info(dump_time(start_time, "Test generation"))
 
         final_stage.final_section()         # post flows?
 
     except Exception as e:
         logger.warning("Test failed :(")
-        dump_time(start_time, "Test total")
+        logger.info(dump_time(start_time, "Test total"))
         logger.error(f"Error: {e}")
         logger.error(traceback.format_exc())
         raise
 
     else:
         # Test was successful
-        dump_time(start_time, "Test total")
+        logger.info(dump_time(start_time, "Test total"))
         logger.info("Test was successful :)\n")
         logger.info("Mission accomplished...")
 
@@ -50,11 +54,10 @@ def main(args=None):
     return True
 
 
-def dump_time(start_time, message_header):
-    logger = get_logger()
+def dump_time(start_time, message_header) -> str:
     current_time = time.time()  # Capture current time
     duration = current_time - start_time # Calculate duration
-    logger.info(f'{message_header} took {duration:.2f} seconds')
+    return f'{message_header} took {duration:.2f} seconds'
 
 
 def set_basedir_path():
@@ -64,6 +67,9 @@ def set_basedir_path():
     - `submodule_content_path`: The resolved path to the submodule content directory.
     The paths are stored in the configuration manager for easy access throughout the application.
     """
+    from Utils.logger_management import get_logger
+    from Utils.configuration_management import get_config_manager
+
     logger = get_logger()
     config_manager = get_config_manager()
 
@@ -72,12 +78,6 @@ def set_basedir_path():
         base_dir = Path(__file__).resolve().parent
         logger.debug(f"Base directory determined: {base_dir}")
         config_manager.set_value('base_dir_path', str(base_dir))
-
-        # # Determine the submodule content path relative to the base directory
-        # submodule_content_path = base_dir / '..' / 'Submodules' / 'arrow_content'
-        # resolved_path = submodule_content_path.resolve()
-        # logger.debug(f"Submodule content path resolved: {resolved_path}")
-        # config_manager.set_value('submodule_content_path', str(resolved_path))
 
     except Exception as e:
         logger.error(f"Error setting base or submodule paths: {e}")
