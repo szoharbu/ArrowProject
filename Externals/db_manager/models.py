a11d00a83306296e8f33320e9dbe8762701a7f21
Zohar Buchris <szoharbu@gmail.com>
2025-01-18 19:20:50 +0200

    add singleton to db_manager

--- Externals/db_manager/models.py
+++ Externals/db_manager/models.py
@@ -2,6 +2,7 @@ import os
 from peewee import Model, CharField, TextField, SqliteDatabase, BooleanField
 from Utils.configuration_management import Configuration
 from Utils.logger_management import get_logger
+from Utils.singleton_management import SingletonManager
 
 """
 File: models.py
